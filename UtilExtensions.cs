6d274de3352d1e2c7c5b06e0001fa99d5f85ac8e
Tamschi <MailTamschi@googlemail.com>
2019-02-12 15:11:19 +0100

    Use unmanaged constraints

--- UtilExtensions.cs
+++ UtilExtensions.cs
@@ -5,8 +5,8 @@ namespace SpanUtils
 {
     public static class UtilExtensions
     {
-        public static ref T View<T>(this Span<byte> @this, int offset) where T : struct => ref MemoryMarshal.Cast<byte, T>(@this.Slice(offset, Marshal.SizeOf<T>()))[0];
-        public static Span<T> ViewRange<T>(this Span<byte> @this, int offset, int count) where T : struct => MemoryMarshal.Cast<byte, T>(@this.Slice(offset, Marshal.SizeOf<T>() * count));
-        public static Span<R> Cast<R>(this Span<byte> @this) where R : struct => MemoryMarshal.Cast<byte, R>(@this);
+        public static ref T View<T>(this Span<byte> @this, int offset) where T : unmanaged => ref MemoryMarshal.Cast<byte, T>(@this.Slice(offset, Marshal.SizeOf<T>()))[0];
+        public static Span<T> ViewRange<T>(this Span<byte> @this, int offset, int count) where T : unmanaged => MemoryMarshal.Cast<byte, T>(@this.Slice(offset, Marshal.SizeOf<T>() * count));
+        public static Span<R> Cast<R>(this Span<byte> @this) where R : unmanaged => MemoryMarshal.Cast<byte, R>(@this);
     }
 }
